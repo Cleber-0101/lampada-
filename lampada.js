d04478ee5cc944c0c34ebd5c96098b0422b59257
cleber <cleberbatista99@hotmail.com>
2022-06-03 21:00:18 -0300

    lampada-JS

---
+++ lampada.js
@@ -0,0 +1,35 @@
+const turnOn = document.getElementById ("turnOn");
+const turnOf = document.getElementById ("turnOf");
+const lamp = document.getElementById ("lamp");
+
+f
+
+
+  function lampOn ()  {
+    
+    lamp.src = './img/ligada.jpg';
+     
+  }
+
+
+
+function lampOf ()  {
+
+    lamp.src = './img/desligada.jpg';
+    }
+
+
+
+function lampBroken () {
+    lamp.src = '/img/quebrada.jpg';
+}
+
+
+
+turnOn.addEventListener ('click' , lampOn);
+turnOf.addEventListener ('click', lampOf);
+lamp.addEventListener ('mouseover' , lampOn);
+lamp.addEventListener ('mouseleave', lampOf);
+lamp.addEventListener('dblclick', lampBroken);
+
+
